a9cd1d5831791bce6e61764c6c5304f2de5dd463
MariaVivancos <mariavivancos@maria.vivancos>
2022-02-23 18:38:10 +0100

    Created show fragment to show the pokemon instead of main page.

--- app/src/main/java/com/example/pokedex/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/pokedex/ui/main/MainFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.example.pokedex.R
 import com.example.pokedex.databinding.FragmentMainBinding
@@ -15,33 +16,16 @@ import com.example.pokedex.mediators.PokemonViewModel
 class MainFragment : Fragment(R.layout.fragment_main) {
 
     lateinit var binding: FragmentMainBinding
-    private val pokemonViewModel by viewModels<PokemonViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Initialise Binding
+        // Binding
         binding = FragmentMainBinding.bind(view)
 
-        // Initialise the call to the Pokemon API
-        pokemonViewModel.getPokemonById((1..1126).random())
-        // Call the observer
-        pokemonObserver()
-    }
-
-    fun pokemonObserver() {
-        pokemonViewModel.response.observe(viewLifecycleOwner) { pokemon ->
-            with(binding) {
-                tvMainPokemonName.text = pokemon.name
-                tvMainPokemonNumber.text = pokemon.id.toString()
-                tvMainPokemonHeight.text = pokemon.height
-                tvMainPokemonWeight.text = pokemon.weight
-                // Image
-
-                Glide.with(requireContext())
-                    .load(pokemon.sprites.other.officialArtwork.front_default)
-                    .dontAnimate()
-                    .into(ivMainPokemonImage)
+        with(binding) {
+            btnMain.setOnClickListener {
+                findNavController().navigate(R.id.action_mainFragment_to_showFragment)
             }
         }
     }
